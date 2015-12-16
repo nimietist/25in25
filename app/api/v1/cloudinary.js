07b67f4cdeae9b506dccb42c1c31c27e64020eb7
John Nguyen <johntn.e3@gmail.com>
2015-12-15 22:33:52 -0800

    Add debug scope

--- app/api/v1/cloudinary.js
+++ app/api/v1/cloudinary.js
@@ -2,11 +2,14 @@ import { Router } from 'express'
 import { default as debugScope } from 'debug'
 import { Artwork } from 'app/models'
 
-const debug = debugScope('cloudinary')
+const debug = debugScope('25in25:cloudinary')
 const cloudinary = new Router()
 
-cloudinary.get('/', (req, res) => {
+cloudinary.all('/', (req, res) => {
   debug('getting', req.query);
+  console.error('getting query', req.query)
+  console.error('getting params', req.params)
+  console.error('getting body', req.body)
   // Artwork.find
   res.send({success: true})
 })
